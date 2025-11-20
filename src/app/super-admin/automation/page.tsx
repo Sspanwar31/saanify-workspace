1b79d3ecb3b44c2889311b16fa5c70de300520bb
Sspanwar31 <s.s.panwar2531@gmail.com>
2025-11-20 23:17:16 +0530

    Create page.tsx

---
+++ src/app/super-admin/automation/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+export default function TestAutomationPage() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1 style={{ color: 'green', fontSize: '30px', fontWeight: 'bold' }}>
+        ✅ Page Found!
+      </h1>
+      <p style={{ fontSize: '18px', marginTop: '10px' }}>
+        आपका फोल्डर स्ट्रक्चर बिल्कुल सही है।
+        <br />
+        (This is inside <code>src/app/super-admin/automation/page.tsx</code>)
+      </p>
+    </div>
+  )
+}
